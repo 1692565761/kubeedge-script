45b448f54f9989120d7b3883c3b04945a36abed1
王书展 <1692565761@qq.com>
2023-05-12 21:43:42 +0800

    fix bugs

--- demo_cloud.sh
+++ demo_cloud.sh
@@ -17,11 +17,6 @@ kubectl get nodes -o wide | awk 'NR>1{print $1"\t\t"$6"\t\t"$2}'
 
 # 让用户输入要部署的节点名，并检查其是否是ready和edge节点
 read -p "请输入要部署到哪个节点：" nodename
-while ! kubectl get node $nodename &> /dev/null || \
-      [[ $(kubectl get nodes -o wide $nodename | awk '{print $3}') != "agent,edge" ]]; do
-    echo "当前节点不是edge节点或者不存在，请重新选择"
-    read -p "请输入要部署到哪个节点：" nodename
-done
 
 # 重写counter-model.yaml文件
 cat > /root/kubeedge-counter-demo/crds/kubeedge-counter-instance.yaml <<EOF
